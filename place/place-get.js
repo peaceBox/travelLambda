2c1fca205cf87fb2f29af907374a83a344b61437
shinbunbun <takamuneyuto@gmail.com>
2020-05-24 01:23:53 +0900

    update place-get

---
+++ place/place-get.js
@@ -0,0 +1,56 @@
+const {
+  v4: uuidv4
+} = require('uuid');
+
+const AWS = require('aws-sdk');
+const dynamoDocument = new AWS.DynamoDB.DocumentClient();
+
+exports.main = async (event) => {
+  const data = JSON.parse(event.body);
+
+  const travelId = data.travelId;
+  const placeId = data.placeId;
+
+  const queryParam = {
+    TableName: 'travelTable',
+    IndexName: 'travelId-dataType-index',
+    KeyConditionExpression: '#k = :val AND #d = :dataType',
+    ExpressionAttributeValues: {
+      ':val': travelId,
+      ':dataType': 'place'
+    },
+    ExpressionAttributeNames: {
+      '#k': 'travelId',
+      '#d': 'dataType',
+    }
+  };
+  const promise = await new Promise((resolve, reject) => {
+    dynamoDocument.query(queryParam, (err, data) => {
+      if (err) {
+        console.log(err);
+        throw new Error(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+  const items = promise.Items;
+  const arr = [];
+  for (const property in items) {
+    if ((items[property]).hasOwnProperty('placeId')) {
+      arr.push({
+        placeId: items[property].startDate
+      });
+    }
+  }
+  const response = {
+    statusCode: 200,
+    headers: {
+      // 'Location': 'https://travel.sugokunaritai.dev',
+      'Access-Control-Allow-Origin': 'http://localhost:8080',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(arr)
+  };
+  return response;
+};
